4079d5fc58c112a17bf06be7f0245f4499c569cb
arith <arithgrey@gmail.com>
2015-06-03 16:19:36 -0500

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -6,8 +6,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'puntoExeJar';
-$db['default']['database'] = 'qrsocial_dev';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
